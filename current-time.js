d6454422dc1a21f7c704b5657853065b66f05f4a
kritiaggarwal <kritiaggarwal@users.noreply.github.com>
2016-06-18 11:24:25 +0530

    Delete current-time.js

--- current-time.js
+++
@@ -1,9 +0,0 @@
-
-function date_time(){
-	var t=new Date();
-	var hours=t.getHours() ;
-	var minutes=t.getMinutes() ;
-	var min = minutes +"";
-    while (min.length < 2) min = "0" + min;
-	document.write("Time is : " + hours + ":" + min );
-}
